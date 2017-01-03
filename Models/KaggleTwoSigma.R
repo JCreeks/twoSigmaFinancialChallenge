ea9cc103bdc4d1c2ab236057c8c7b48d106dc51a
jing guo <jingguo@jings-MacBook-Air.local>
2017-01-03 07:36:03 -0500

    1/2/16

--- Models/KaggleTwoSigma.R
+++ Models/KaggleTwoSigma.R
@@ -1,7 +1,20 @@
+rm(list=ls(all=TRUE)) 
+cat("\014") 
+
 source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
 
 library(rhdf5)
-h5ls("Desktop/train.h5")
-mydata <- h5read("Desktop/train.h5", "foo/A")
-str(mydata)
+h5ls("/Users/jingguo/Desktop/KaggleCompetition/input/train.h5")
+#mydata <- h5read("/Users/jingguo/Desktop/KaggleCompetition/input/train.h5",
+#                 'train/')
+
+df=data.frame(t(h5read("/Users/jingguo/Desktop/KaggleCompetition/input/train.h5",
+                       'train/block0_values')),
+              t(h5read("/Users/jingguo/Desktop/KaggleCompetition/input/train.h5",
+                     'train/block1_values')))
+colnames(df)=c(h5read("/Users/jingguo/Desktop/KaggleCompetition/input/train.h5",
+                      'train/block0_items'),
+               h5read("/Users/jingguo/Desktop/KaggleCompetition/input/train.h5",
+                      'train/block1_items')
+               )
