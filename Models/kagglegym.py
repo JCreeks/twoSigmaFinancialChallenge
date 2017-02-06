d8e865a72ceb45c7f0a7e8abdcc8c3f4fe50bee4
jing guo <jingguo@dyn-209-2-214-48.dyn.columbia.edu>
2017-02-05 19:38:00 -0500

    xxx

--- Models/kagglegym.py
+++ Models/kagglegym.py
@@ -1,77 +1,10 @@
-# This file minics the kagglegym api, including scoring.
-#
-# You can use it to run experiments locally on the sample training set..
-# To do this follow the following steps.
-#
-# Create the following directory structure
-# .
-# +--input
-# |  +- train.h5
-# +--Models
-#    +- kagglegym.py
-#    +- __init__.py
-#    +- test_gym.py
-#
-# Where
-#
-# - train.h5 contains the unzipped data for the competition.
-#
-# - kagglegym.py is this file
-#
-# - test_gym.py  is a file with the following content:
-#
-#    import kagglegym
-#
-#    env = kagglegym.make()
-#    observation = env.reset()
-#
-#    print(len(observation.target))
-#    print(len(observation.train))
-#
-#    n = 0
-#    rewards = []
-#    while True:
-#        target = observation.target
-#        target.loc[:, 'y'] = 0.01
-#        observation, reward, done, info = env.step(target)
-#        if done:
-#            break
-#        rewards.append(reward)
-#        n = n + 1
-#
-#    print(info)
-#    print(n)
-#    print(rewards[0:15])
-#
-# Running
-#    python test_gym.py
-#
-# Should result in:
-#
-#  968
-#  806298
-#  {'public_score': -0.42791846067884648}
-#  906
-# [-0.57244240701870674, -0.69602355761934143, -0.76969605094330085,
-# -0.88292556152797097, -0.77409471948462827, -0.62031952966389658,
-# -0.39146488525587475, -0.84779897534426341, -0.45051451441360757,
-# -0.56453402316570156, -0.89329857536073409, -0.81062686366326098,
-# -0.61593843763923251, -0.67321374079183682, -0.82632908490446888]
-#
-# Revision history:
-# 1. Minimal verion posted by Frans Slothouber on the forum.
-#    see link https://www.kaggle.com/c/two-sigma-financial-modeling/discussion/26044#148202
-# 2. Modification by Devin that add score
-#    Parent of this fork
-# 3. Bug fix for the scoring function, and documentation
-#    This fork
-# 4. Bug fix for computation of final score. Thanks to PeterBruhn and vgoklani
-# 5. Fix in reset function, thanks to Devin
-#
-
+import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score
+
+# This is taken from Frans Slothoubers post on the contest discussion forum.
+# https://www.kaggle.com/slothouber/two-sigma-financial-modeling/kagglegym-emulation
 
 
 def r_score(y_true, y_pred, sample_weight=None, multioutput=None):
@@ -92,6 +25,10 @@ class Observation(object):
 
 
 class Environment(object):
+    ID_COL_NAME = "id"
+    SAMPLE_COL_NAME = "sample"
+    TARGET_COL_NAME = "y"
+    TIME_COL_NAME = "timestamp"
     def __init__(self):
         with pd.HDFStore("../input/train.h5", "r") as hfdata:
             self.timestamp = 0
@@ -175,3 +112,26 @@ class Environment(object):
 
 def make():
     return Environment()
+
+def test():
+    env = make()
+    observation = env.reset()
+
+    print(len(observation.target))
+    print(len(observation.train))
+
+    n = 0
+
+    rewards = []
+    while True:
+        target = observation.target
+        target.loc[:, 'y'] = 0.01
+        observation, reward, done, info = env.step(target)
+        if done:
+            break
+        rewards.append(reward)
+        n = n + 1
+
+    print(info)
+    print(n)
+    print(rewards[0:15])
