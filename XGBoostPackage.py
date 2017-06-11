8e75c162486b0a56fd39312d5f7f51b8732a4e7e
jing guo <jingguo@jings-MacBook-Air.local>
2017-06-11 11:35:14 -0400

    jun11

--- XGBoostPackage.py
+++ XGBoostPackage.py
@@ -117,17 +117,17 @@ class xgbClass(object):
         return self.model.predict(dtest)
 
 
-def modelfit(alg, dtrain, predictors, target, metrics, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
+def modelfit(alg, dtrain, predictors, target, CVmetrics, XGBmetrics='rmse', useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
     
     if useTrainCV:
         xgb_param = alg.get_xgb_params()
         xgtrain = xgb.DMatrix(dtrain[predictors].values, label=dtrain[target].values)
         cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
-            metrics=metrics, early_stopping_rounds=early_stopping_rounds)#, show_progress=False)
+            metrics=CVmetrics, early_stopping_rounds=early_stopping_rounds)#, show_progress=False)
         alg.set_params(n_estimators=cvresult.shape[0])
     
     #Fit the algorithm on the data
-    alg.fit(dtrain[predictors], dtrain['Disbursed'],eval_metric='auc')
+    alg.fit(dtrain[predictors], dtrain[target],eval_metric=XGBmetrics)
         
     #Predict training set:
     dtrain_predictions = alg.predict(dtrain[predictors])
@@ -135,8 +135,8 @@ def modelfit(alg, dtrain, predictors, target, metrics, useTrainCV=True, cv_folds
         
     #Print model report:
     print "\nModel Report"
-    print "Accuracy : %.4g" % metrics.accuracy_score(dtrain['Disbursed'].values, dtrain_predictions)
-    print "AUC Score (Train): %f" % metrics.roc_auc_score(dtrain['Disbursed'], dtrain_predprob)
+    print "CV metrics : %.4g" % CVmetrics(dtrain[target].values, dtrain_predictions)
+    #print metrics+" Score (Train): %f" % metrics.roc_auc_score(dtrain['Disbursed'], dtrain_predprob)
                     
     feat_imp = pd.Series(alg.booster().get_fscore()).sort_values(ascending=False)
     feat_imp.plot(kind='bar', title='Feature Importances')
